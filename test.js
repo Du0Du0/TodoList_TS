39b199ea1c9189918136e87882a2b6479c58dad3
do-hyun,kim <dohyun911@hanmail.net>
2023-09-06 11:16:21 +0900

    feature: output line to li element when check box

--- test.js
+++ test.js
@@ -17,7 +17,7 @@ form.addEventListener('submit', (e) => {
 
 	input.value = '';
 
-	tasks.push(newTask);
+	tasks = [newTask, ...tasks];
 	console.log('newTask', newTask);
 
 	list.innerHTML = '';
@@ -30,6 +30,15 @@ function addListItem(task) {
 	const checkbox = document.createElement('input');
 	//input노드에 checkbox타입 설정
 	checkbox.type = 'checkbox';
+
+	//동적으로 생성되는 checkbox요소에 아예 이벤트핸들러까지 연결해서 생성
+	//이벤트위임을 하지 않아도 동적인 요소에 이벤트 연결하는 방법
+	checkbox.addEventListener('change', () => {
+		task.complete = checkbox.checked;
+		//change이벤트가 발생할때마다 해당객체의 complete값이 true면 line-through적용 그렇지 않으면 미적용
+		item.style.textDecoration = task.complete ? 'line-through' : 'none';
+	});
+
 	//li노드에  자식으로 checkbox, 인수로 받은 객체의 할일내용 추가
 	item.append(checkbox, task.title);
 	//완성된 li노드를 ul안에 추가
